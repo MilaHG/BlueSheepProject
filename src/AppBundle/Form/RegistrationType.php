294834932d6768c4cfae8f90ad606a4bfbfb2408
dia42r <dia42r@gmail.com>
2017-01-03 11:06:26 +0100

    Install and config FOSUserBundle.add views files

---
+++ src/AppBundle/Form/RegistrationType.php
@@ -0,0 +1,110 @@
+<?php
+
+/*
+ * This file is part of the FOSUserBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Form;
+
+use FOS\UserBundle\Util\LegacyFormHelper;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationType extends AbstractType
+{
+
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+
+        // Or for Symfony < 2.8
+        // return 'fos_user_registration';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('gender',  
+                    TextType::class,
+                    array(
+                        'label' => 'Civilite'
+                        ))  
+                
+            ->add('firstname',
+            TextType::class,
+                    array(
+                        'label' => 'Nom'
+                    ))  
+                
+            ->add('lastname',
+            TextType::class,
+                    array(
+                        'label' => 'Prenom'
+                    ))
+            
+            ->add('birthdate',
+            \Symfony\Component\Form\Extension\Core\Type\DateType::class,
+                    array(
+                        'label' => 'Date de naissance'
+                    ))
+            ->add('address',
+            TextType::class,
+                    array(
+                        'label' => 'Adresse'
+                    )) 
+                
+            ->add('zip',
+            TextType::class,
+                    array(
+                        'label' => 'Code postal'
+                    )) 
+                
+            ->add('photo',
+            \Symfony\Component\Form\Extension\Core\Type\FileType::class,
+                    array(
+                        'label' => 'Photo',
+                        'required' => FALSE
+                    ))    
+                
+            ->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'), array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'))
+            ->add('username', null, array('label' => 'form.username', 'translation_domain' => 'FOSUserBundle'))
+            ->add('plainPassword', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\RepeatedType'), array(
+                'type' => LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'),
+                'options' => array('translation_domain' => 'FOSUserBundle'),
+                'first_options' => array('label' => 'form.password'),
+                'second_options' => array('label' => 'form.password_confirmation'),
+                'invalid_message' => 'fos_user.password.mismatch',
+            ))
+        ;
+    }
+
+ 
+
+    // BC for SF < 3.0
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'app_user_registration';
+    }
+}
