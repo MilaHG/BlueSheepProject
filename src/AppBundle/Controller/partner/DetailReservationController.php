aae70505dd7c3af69d15c69236eb8be7696004a0
Florent <mallet.florent@outlook.fr>
2017-01-09 10:33:27 +0100

    last modif before merge (reservation controller)

--- src/AppBundle/Controller/partner/DetailReservationController.php
+++ src/AppBundle/Controller/partner/DetailReservationController.php
@@ -52,6 +52,89 @@ class DetailReservationController extends Controller {
 		$reservation = $d_reservation->getReservation();
 //		$user=$reservation->getUser();
 		
+		//**preparation du mail de contact :
+		
+		$formBuilder = $this->createFormBuilder();
+		//ici on ne fait pas de form type car on n'a pas de lien avec la base de données, donc pas de mapping
+		
+		$formBuilder
+			->add(
+				'name',
+				TextType::class,
+				[
+				  'label'=>'Nom',
+				  'constraints' => new NotBlank
+				]
+			)
+			->add(
+				'email',
+				EmailType::class,
+				[
+				  'label'=>'Nom',
+				  'constraints' =>
+				  [
+					new NotBlank(),
+					new Email(),
+				  ]
+				]
+			)
+			->add(
+				'subject',
+				TextType::class,
+				[
+				  'label'=>'Sujet',
+				  'constraints' => new NotBlank
+				]
+			)
+			->add(
+				'body',
+				TextareaType::class,
+				[
+				  'label'=>'Votre message',
+				  'constraints' => new NotBlank
+				]
+			)
+		;
+		
+		$form = $formBuilder->getForm();
+		
+		if($this->getUser()){
+			$form->get('name')->setData($this->getUser()->getFullName());
+			$form->get('email')->setData($this->getUser()->getEmail());
+		}
+		
+		$form->handleRequest($request);
+		if ($form->isSubmitted()){
+			if($form->isValid()){
+				$data = $form->getData();
+							
+				/** \Swift_Mailer */
+				$mailer = $this->get('mailer');
+				/* les config du mailer doivent etre config/ecrite dans parametres.ymail
+				 * Attention : on ne versionne pas les paramètres, mais on rajoute des infos dans parametres.ymail.dist
+				 */
+				
+				$mail = $mailer->createMessage();
+				
+				$mailContent = "<h3>Blue-Sheep : message de ${data['name']} (${data['email']})" . '<p><b>' . $data['subject'] 
+					. '</b></p>' . '<p>' . nl2br($data['body']) . '</p>'
+				;
+				
+				$mail
+					->setSubject('Blue-sheep : Message from our partner about your reservation ')
+					->setFrom($this->getParameter('contact_email'))
+					->setTo($this->getParameter('contact_email'))
+					->setBody($mailContent)
+					->setReplyTo($data['email'])
+				;
+				
+				$mailer->send($mail);
+				$this->addFlash('success', 'email sent');
+			}
+			else{
+				$this->addFlash('error', 'your email couldn\'t be sent' );
+			}
+		}
 		
 		return $this->render(
 			'Partner/DetailReservation/view.html.twig', 
@@ -59,6 +142,7 @@ class DetailReservationController extends Controller {
 			  'd_reservation'	=>$d_reservation,
 			  'reservation'	=>$reservation,
 //			  'user'		=>$user,
+//			  'form' => $form->createView(),
 			]
 		);
 	}	
